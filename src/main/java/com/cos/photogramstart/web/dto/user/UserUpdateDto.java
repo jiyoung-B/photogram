e37986bdf5f90e467837d67b1a18e92607587819
jiyoung-B <catgirl0313@naver.com>
2023-08-11 23:28:56 +0900

    commit

--- src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/user/UserUpdateDto.java
@@ -1,5 +1,28 @@
 package com.cos.photogramstart.web.dto.user;
 
-public class UserUpdateDto {
 
+
+import com.cos.photogramstart.domain.user.User;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateDto {
+	private String name;
+	private String password;
+	private String website;
+	private String bio;
+	private String phone;
+	private String gender;
+	
+	public User toEntity() {
+		return User.builder()
+				.name(name)
+				.password(password)
+				.website(website)
+				.bio(bio)
+				.phone(phone)
+				.gender(gender)
+				.build();
+	}
 }
